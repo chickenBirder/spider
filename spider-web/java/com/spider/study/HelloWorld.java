ad280470a8964ce8784ce611346fe28236b1948a
chickenBirder <36870619+chickenBirder@users.noreply.github.com>
2018-10-23 22:00:57 +0800

    Update HelloWorld.java
    add one message.

--- spider-web/java/com/spider/study/HelloWorld.java
+++ spider-web/java/com/spider/study/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("yes,this is your first data!keep yourself.")
 	}
 
 }
